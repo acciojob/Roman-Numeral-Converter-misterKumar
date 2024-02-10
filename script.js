0838aaff0615ae3c4daa2c41f2814e83f2d466ec
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-10 20:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-function convertToRoman(num) {
-  const obj = {
-    1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
-    100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
-    10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
-  };
+/*function convertToRoman(num) {
+  	const obj = {
+      0:['M',1000], 
+	  1:['CM',900],
+      2:['D', 500], 
+	  3:['CD',400],
+      4:['C', 100], 
+	  5:['XC',90],
+      6:['L', 50],
+	  7:['XL',40],
+      8:['X', 10],
+	  9:['IX',9],
+      10:['V', 5], 
+	  11:['IV',4],
+      12:['I', 1]
+    };
 
-  let result = '';
+  //your code here
+	let str="";
+	
+	for(let i in obj){
+		let quo=0;
+		if(num>=obj[i][1]){
+			quo=Math.floor(num/obj[i][1]);
+			num=num-(obj[i][1]*quo);
+		}
+		for(let j=0;j<quo;j++){
+			str+=obj[i][0];
+		}
+	}
+	return str;
 
-  for (let key in obj) {
-    while (num >= key) {
-      result += obj[key];
-      num -= key;
-    }
-  }
-
-  return result;
 }
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(14)); // Output: XIV
-console.log(convertToRoman(798)); // Output: DCCXCVIII
+// console.log(convertToRoman(36));
+
+
+
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman*/
+
+
